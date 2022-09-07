dcdf0381e91a87cc167039b7801fe7fd19b08e31
AmritUdasi <audasi@deqode.com>
2022-09-07 16:40:25 +0530

    first release

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-function sayhello(name){
-    console.log("hello "+name)
-
+var prompt = require('prompt-sync')();
+var num = prompt('Enter a 1st number: ');
+var prompt2 = require('prompt-sync')();
+var ope = prompt('Enter a operator: ');
+var prompt1 = require('prompt-sync')();
+var num3 = prompt('Enter a 2nd number: ');
+if(ope=='+'){
+    var total=Number(num)+Number(num3);    
+}else if(ope=='-'){
+    var total=Number(num)-Number(num3); 
+}else if(ope=='/'){
+    var total=Number(num)/Number(num3); 
+}else if(ope=='*'){
+    var total=Number(num)*Number(num3); 
 }
-function add(a,b){
-    console.log(a+b);
-}
-export{sayhello};
-export{add};
+console.log(total);
